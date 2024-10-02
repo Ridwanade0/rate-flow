cf915d942113ae68b082677237e310a8c442aabf
Ridwanade0 <royeniyiadegboyega@gmail.com>
2024-10-02 16:39:07 +0100

    added a index page for api explanation next up i sdocumentation improvement, added some now but not great

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ import express from "express";
 import "dotenv/config";
 import currencyRouterV1 from "./routers/currencyRouterV1.js";
 import connectDB from "./lib/connectDB.js";
+import rateLimit from "express-rate-limit";
+import path from "path";
+import { fileURLToPath } from 'url';
+// Get __dirname equivalent in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // added a 15 minutes window
+    max: 80, //50 request per window for each IP adress
+    message: {
+        status: 429,
+        sucess: false,
+        message: "To many requests, please try again later."
+    }
+});
 
 //Self executing function to execute start the application.
 (async function () {
@@ -12,11 +27,14 @@ const PORT = process.env.PORT || 3000;
     app.set("json spaces", 2); // add spacing to all json response (prrety-print)
 
     app.get("/", (req, res) => {
-        res.status(200).send("Rate-Flow server now available");
+        res.sendFile(path.join(__dirname, "public", "index.html"));
+    });
+    app.get("/documentation", (req, res) => {
+        res.sendFile(path.join(__dirname, "public", "documentation.html"));
     });
 
     // Routes for currency API, versioned (v1)
-    app.use("/api/v1", currencyRouterV1);
+    app.use("/api/v1", limiter, currencyRouterV1);
 
     app.listen(PORT, () => {
         console.log(`Server is listening at http://localhost:${PORT}`);
