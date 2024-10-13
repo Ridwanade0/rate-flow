e3e8206ebc6a28a55c9686303babb298ce7fbaef
Ridwanade0 <royeniyiadegboyega@gmail.com>
2024-10-13 22:56:30 +0100

    completed latest.json rates fetching, muliple fetches and symbols specifications

--- src/controller/latestRatesController.ts
+++ src/controller/latestRatesController.ts
@@ -3,8 +3,9 @@ import latestRatesService from "../service/latestRatesService";
 
 const latestRatesController = async (req: Request, res: Response) => {
   try {
-    const base = typeof req.query.base === 'string' ? req.query.base : 'USD';
-    const rates = await latestRatesService(base.toUpperCase());
+    const base = typeof req.query.base === "string" ? req.query.base : 'USD';
+    const symbols = typeof req.query.symbols === "string" ? req.query.symbols : "";
+    const rates = await latestRatesService(base.toUpperCase(), symbols.toUpperCase());
     res.status(200).json(rates);
   } catch (error) {
     const err = error as Error; // Cast error to Error type
